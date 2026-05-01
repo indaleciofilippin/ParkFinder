23a2100c689101d6f8bfba6fd20f04328c0b9748
indaleciofilippin <80007395+indaleciofilippin@users.noreply.github.com>
2026-05-01 11:22:41 -0300

    Revert "PARkFINDER 11"

--- Backend/app/models/base_model.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, DateTime, func
-
-Base = declarative_base()
-
-class BaseModel(Base):
-    __abstract__ = True
-
--- Backend/app/models/role.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .base_model import BaseModel
-
-class Role(BaseModel):
-    __tablename__ = "role"
-    id_role = Column(Integer, primary_key=True, index=True)
-    name = Column(String(10), unique=True, nullable=False)
