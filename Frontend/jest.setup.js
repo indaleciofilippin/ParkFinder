8ec84ff2b5464dbfc0bebab80c0f2083d12d6ff6
leonardomorabito02 <leonardomorabito02@gmail.com>
2026-06-04 23:13:25 -0300

    fix test

--- Frontend/jest.setup.js
+++ Frontend/jest.setup.js
@@ -1,3 +1,64 @@
+jest.mock('react-native/jest/mockComponent', () => {
+  return {
+    __esModule: true,
+    default: (moduleName, instanceMethods, isESModule) => {
+      const React = require('react');
+      const RealComponent = isESModule
+        ? jest.requireActual(moduleName).default
+        : jest.requireActual(moduleName);
+
+      const SuperClass =
+        typeof RealComponent === 'function' &&
+        RealComponent.prototype &&
+        RealComponent.prototype.constructor instanceof React.Component
+          ? RealComponent
+          : React.Component;
+
+      const name =
+        RealComponent.displayName ??
+        RealComponent.name ??
+        (RealComponent.render == null
+          ? 'Unknown'
+          : RealComponent.render.displayName ?? RealComponent.render.name);
+
+      const nameWithoutPrefix = name.replace(/^(RCT|RK)/, '');
+
+      const Component = class extends SuperClass {
+        static displayName = 'Component';
+        render() {
+          const props = { ...RealComponent.defaultProps };
+          if (this.props) {
+            Object.keys(this.props).forEach(prop => {
+              if (this.props[prop] !== undefined) {
+                props[prop] = this.props[prop];
+              }
+            });
+          }
+          return React.createElement(nameWithoutPrefix, props, this.props.children);
+        }
+      };
+
+      Object.defineProperty(Component, 'name', {
+        value: name,
+        writable: false,
+        enumerable: false,
+        configurable: true,
+      });
+
+      Component.displayName = nameWithoutPrefix;
+
+      Object.keys(RealComponent).forEach(classStatic => {
+        Component[classStatic] = RealComponent[classStatic];
+      });
+
+      if (instanceMethods != null) {
+        Object.assign(Component.prototype, instanceMethods);
+      }
+
+      return Component;
+    },
+  };
+});
 
 jest.mock('@react-native-google-signin/google-signin', () => ({
   GoogleSignin: {
